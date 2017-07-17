131f9af7d1109f5392ed8286557ecbe69e691cc0
hkadivar <hkadivar@unc.edu>
2017-07-17 19:00:07 -0400

    updating all repositories

---
+++ scripting/hello-world.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Hello, world!"
+# say "Hello, world!"
+echo "These are the files in this directory:"
+echo ""
+echo "`ls`"
+echo "You are in this directory:"
+echo ""
+echo "`pwd`"
+echo ""
+echo "I am `whoami` from `hostname`"
+echo ""
